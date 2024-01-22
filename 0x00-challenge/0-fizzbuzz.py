889189dea07a81bde5a03345eb5b0e928e2b3791
Ayokayzy <theayokayzy1@gmail.com>
2024-01-22 15:38:39 +0000

    chmod

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -25,7 +25,7 @@ def fizzbuzz(n):
             tmp_result.append("Buzz")
         else:
             tmp_result.append(str(i))
-    print(" ".join(tmp_result), end="")
+    print(" ".join(tmp_result))
 
 
 if __name__ == '__main__':
